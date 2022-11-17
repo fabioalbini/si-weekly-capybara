4df9ad4fbd3bafed7d8d5404f181e31735023f07
Fábio Albini Braga <fabio.albini@diffagency.com>
2022-11-17 18:25:46 -0500

    adds blank spec

---
+++ spec/system/products/product_details_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Products / Product Details', type: :system do
+  it 'logs in successfully' do
+    # TODO
+    # Go to the product listing page
+    # Search for Product 20
+    # Click on link
+    # Ensure details are present for selected product
+  end
+end
